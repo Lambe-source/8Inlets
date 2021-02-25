d8f0b6e2d7636ae5f374009268263a8b3a2d054f
Janessa Ridley <jn358585@dal.ca>
2021-02-25 12:15:02 -0400

    Renamed app.py as required for Elastic Beanstalk. Created zip folder to create EB environment.

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, render_template
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 @app.route('/', methods=['GET'])
