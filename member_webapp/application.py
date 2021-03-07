8bc17445becb1a0991ad84dd9dc609ba25708932
Janessa Ridley <jn358585@dal.ca>
2021-03-07 13:22:06 -0400

    Added returning member form elements, added dynamodb requirements. Bug: dynamodb query key related errors

--- member_webapp/application.py
+++ member_webapp/application.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask, jsonify, render_template, redirect
+import boto3
+from flask import Flask, jsonify, render_template, redirect, request
+from boto3.dynamodb.conditions import Key
 
 application = app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
 
 @app.route('/', methods=['GET'])
 def home():
@@ -17,6 +21,27 @@ def new_member():
 @app.route('/returning_member', methods=['POST'])
 def returning_member():
     return render_template('returning_member.html')
+
+# Route to verfity returning member exists in dynamoDB
+@app.route('/verify', methods=['POST'])
+def verify_user():
+    # message use from: https://overiq.com/flask-101/form-handling-in-flask/
+    message = ""
+    if request.method == 'POST':
+        name = request.form['name']
+        phone_number = request.form['phone_number']
+
+        table = dynamodb.Table('Members')
+        response = table.query(
+            KeyConditionExpression=Key('Name').eq(name) & Key('PhoneNo').eq(phone_number)
+        )
+        items = response['Items']
+        user = items[0]
+        if user:
+            message = "Log in successful!"
+        else:
+            message = "User not found!"
+    return render_template('returning_member.html', message=message)
 
 # Route for staff members 
 @app.route('/staff_portal', methods=['GET'])
